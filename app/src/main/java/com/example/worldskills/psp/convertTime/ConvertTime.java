db4b92cb568435a99327b56529f43baa4daa85d8
Daniel <dani@gmail.com>
2018-09-01 09:01:26 -0500

    d

--- app/src/main/java/com/example/worldskills/psp/convertTime/ConvertTime.java
+++ app/src/main/java/com/example/worldskills/psp/convertTime/ConvertTime.java
@@ -1,4 +1,17 @@
 package com.example.worldskills.psp.convertTime;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class ConvertTime {
+    String [] timeStart,timeEnd;
+    DateFormat dateFormat=new SimpleDateFormat("HH:mm:ss");
+    public String cambiarTiempo(Date dateStart,Date dateEnd,int interruption){
+        dateFormat.format(dateStart).split(":");
+
+
+        return "";
+    }
 }
