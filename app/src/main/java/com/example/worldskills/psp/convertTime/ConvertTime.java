d1d6f6729790620e64fcb0120d3c0c2269a26457
Daniel <dani@gmail.com>
2018-09-01 09:36:12 -0500

    d

--- app/src/main/java/com/example/worldskills/psp/convertTime/ConvertTime.java
+++ app/src/main/java/com/example/worldskills/psp/convertTime/ConvertTime.java
@@ -9,7 +9,25 @@ public class ConvertTime {
     String [] timeStart,timeEnd;
     DateFormat dateFormat=new SimpleDateFormat("HH:mm:ss");
     public String cambiarTiempo(Date dateStart,Date dateEnd,int interruption){
-        dateFormat.format(dateStart).split(":");
+        timeStart=dateFormat.format(dateStart).split(":");
+        timeEnd=dateFormat.format(dateEnd).split(":");
+        int resultado=-1;
+        int horas1=((Integer.valueOf(timeStart[0]))/(60*60000))%24;
+        int horas2=((Integer.valueOf(timeEnd[0]))/(60*60000))%24;
+        int min1=((Integer.valueOf(timeStart[1]))/(60000))%60;
+        int min2=((Integer.valueOf(timeEnd[1]))/(60000))%60;
+        int seg1=((Integer.valueOf(timeStart[2]))/(1000))%60;
+        int seg2=((Integer.valueOf(timeEnd[2]))/(1000))%60;
+        int millis1=horas1+min1+seg1;
+        int millis2=horas2+min2+seg2;
+        if(millis2<millis1 && (millis2-millis1)>(interruption*60000)) {
+            resultado = (millis2 - millis1) - (interruption * 60000);
+        }else return "Algun dato no cumple las condiciones";
+        return cambiarMillis(resultado);
+
+    }
+    private String  cambiarMillis(int resultado){
+
 
 
         return "";
